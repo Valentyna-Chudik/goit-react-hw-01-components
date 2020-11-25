9a90c96375872f354165465a985717d89f7ee066
Valentyna-Chudik <valentyna_chudik@ukr.net>
2020-11-25 17:46:38 +0200

    Updates

---
+++ static/js/main.f4f69d87.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],[function(o,t){}],[[0,1]]]);
+//# sourceMappingURL=main.f4f69d87.chunk.js.map
